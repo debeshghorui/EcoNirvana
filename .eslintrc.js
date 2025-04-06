ced9f493c59aacb18a47bccc5d912f5a267f177f
debeshghorui <debeshghorui@gmail.com>
2025-04-06 11:08:37 +0530

    Update ESLint rules to disable warnings and add ESLint configuration to ignore errors during production builds

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,8 @@ module.exports = {
   ],
   rules: {
     'react/no-unescaped-entities': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    'react-hooks/exhaustive-deps': 'warn'
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/exhaustive-deps': 'off'
   }
 }; 
